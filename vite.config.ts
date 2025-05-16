86cf332edaeecb42551e263b6199c423485cc41d
Lynoma <85151448+Lynoma@users.noreply.github.com>
2025-05-16 14:03:06 +0200

    First commit

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/",
+  base: "/portolfio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
